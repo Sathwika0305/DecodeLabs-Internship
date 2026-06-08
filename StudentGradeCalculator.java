7b05022713f63591a1ac184a192f5f60fd46c9e1
Nalam Sathwika <nalamsathwika@gmail.com>
2026-06-08 20:10:34 +0530

    Add files via upload

---
+++ StudentGradeCalculator.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class StudentGradeCalculator {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of subjects: ");
+        int numberOfSubjects = sc.nextInt();
+
+        int totalMarks = 0;
+
+        for (int i = 1; i <= numberOfSubjects; i++) {
+            int marks;
+
+            while (true) {
+                System.out.print("Enter marks for Subject " + i + " (0-100): ");
+                marks = sc.nextInt();
+
+                if (marks >= 0 && marks <= 100) {
+                    break;
+                } else {
+                    System.out.println("Invalid marks! Please enter marks between 0 and 100.");
+                }
+            }
+
+            totalMarks += marks;
+        }
+
+        double averagePercentage = (double) totalMarks / numberOfSubjects;
+
+        char grade;
+
+        if (averagePercentage >= 90) {
+            grade = 'A';
+        } else if (averagePercentage >= 80) {
+            grade = 'B';
+        } else if (averagePercentage >= 70) {
+            grade = 'C';
+        } else if (averagePercentage >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+
+        System.out.println("\n===== STUDENT RESULT =====");
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.printf("Average Percentage: %.2f%%\n", averagePercentage);
+        System.out.println("Grade: " + grade);
+
+        sc.close();
+    }
+}
